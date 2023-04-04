d88a36c52caefaa5220082145dcefc2aaa10b59a
stefanoreddd <stefanogg95@gmail.com>
2023-04-04 21:22:27 +0200

    bonus add button completed.

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 let listaSpesa = ["Latte", "Uova", "Burro", "Pane", "Caffè"];
 
-/*for (i = 0; i < listaSpesa.length; i++){
-    console.log(listaSpesa[i]);
+for (i = 0; i < listaSpesa.length; i++){
+    console.log(i+1 + ") " + listaSpesa[i]);
+    document.getElementById("listaDellaSpesa").innerHTML += `<li>${listaSpesa[i]}</li>`
 }
-*/
 
-let i = 0
+
+/*let i = 0
 
 while (i < listaSpesa.length){
     console.log(i+1 + ") " + listaSpesa[i]);
     i++;
+}*/
+
+function spesaButton () {
+    let spesaAddOne = document.getElementById("addItem").value;
+
+    listaSpesa.push(spesaAddOne);
+
+    document.getElementById("listaDellaSpesa").innerHTML += `<li>${listaSpesa[(listaSpesa.length - 1)]}</li>`;
 }
 
+document.getElementById("buttonAdd").addEventListener("click", spesaButton);
 
 
 
