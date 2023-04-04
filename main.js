745fd3c96365be5237fe997e88baccbc662abde7
stefanoreddd <stefanogg95@gmail.com>
2023-04-04 18:35:18 +0200

    cycles completed. Bonus to be done.

---
+++ main.js
@@ -0,0 +1,19 @@
+let listaSpesa = ["Latte", "Uova", "Burro", "Pane", "Caffè"];
+
+/*for (i = 0; i < listaSpesa.length; i++){
+    console.log(listaSpesa[i]);
+}
+*/
+
+let i = 0
+
+while (i < listaSpesa.length){
+    console.log(i+1 + ") " + listaSpesa[i]);
+    i++;
+}
+
+
+
+
+
+
